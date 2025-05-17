71b32768176c4ac468dd41f238e02a38de18c949
AlexSmetanin <AlexSmetanin72@gmail.com>
2025-05-17 23:50:14 +0300

    Rename Halls

---
+++ Halls/ukrsong.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+      const soundFile = card.getAttribute('data-sound');
+      if (soundFile) {
+        const audio = new Audio('CSS/audio/' + soundFile);
+        audio.play();
+      }
+    });
+  });
